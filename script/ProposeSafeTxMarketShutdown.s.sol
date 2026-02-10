443395c8cdd58e12d0df0199d256b7e70330d889
Antonio Viggiano <agfviggiano@gmail.com>
2026-02-10 12:50:49 -0300

    ci: remove upgrade-related fork tests (#8)
    - Drop fork tests that execute upgrade scripts (rheo-fm not deployed yet)\n- Fix forge fmt import ordering

--- script/ProposeSafeTxMarketShutdown.s.sol
+++ script/ProposeSafeTxMarketShutdown.s.sol
@@ -4,10 +4,10 @@ pragma solidity 0.8.23;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-import {Safe} from "@safe-utils/Safe.sol";
 import {BaseScript} from "@rheo-fm/script/BaseScript.sol";
 import {GetMarketShutdownCalldataScript} from "@rheo-fm/script/GetMarketShutdownCalldata.s.sol";
 import {Contract, Networks} from "@rheo-fm/script/Networks.sol";
+import {Safe} from "@safe-utils/Safe.sol";
 
 import {IRheoFactory} from "@rheo-fm/src/factory/interfaces/IRheoFactory.sol";
 
