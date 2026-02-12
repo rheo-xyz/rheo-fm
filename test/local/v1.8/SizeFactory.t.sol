a9edc5d5644eb876e99d80e287a89b5e9f917bf4
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2026-02-12 10:21:37 -0300

    test: raise factory coverage for coveralls

---
+++ test/local/v1.8/SizeFactory.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {SizeFactory} from "@rheo-fm/src/factory/SizeFactory.sol";
+import {BaseTest} from "@rheo-fm/test/BaseTest.sol";
+import {RheoMock} from "@rheo-fm/test/mocks/RheoMock.sol";
+
+contract SizeFactoryTest is BaseTest {
+    function test_SizeFactory_setSizeImplementation_updates_aliases() public {
+        SizeFactory factory = SizeFactory(payable(address(sizeFactory)));
+        address newImplementation = address(new RheoMock());
+
+        factory.setSizeImplementation(newImplementation);
+
+        assertEq(factory.sizeImplementation(), newImplementation);
+        assertEq(factory.rheoImplementation(), newImplementation);
+    }
+
+    function test_SizeFactory_markets_registry_helpers() public {
+        SizeFactory factory = SizeFactory(payable(address(sizeFactory)));
+
+        assertTrue(factory.isRheoMarket(address(size)));
+        assertEq(factory.getMarketsCount(), 1);
+
+        address[] memory markets = factory.getMarkets();
+        assertEq(markets.length, 1);
+        assertEq(markets[0], address(size));
+
+        factory.removeMarket(address(size));
+
+        assertFalse(factory.isRheoMarket(address(size)));
+        assertEq(factory.getMarketsCount(), 0);
+        assertEq(factory.getMarkets().length, 0);
+    }
+
+    function test_SizeFactory_upgradeToAndCall_authorized() public {
+        SizeFactory factory = SizeFactory(payable(address(sizeFactory)));
+        address newImplementation = address(new SizeFactory());
+
+        factory.upgradeToAndCall(newImplementation, bytes(""));
+
+        assertEq(factory.getMarketsCount(), 1);
+    }
+}
